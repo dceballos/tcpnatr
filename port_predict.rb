6ebcea3fb5d86b0feb9559bf47a250ec13f42492
Daniel Ceballos <daniel@Daniel-Ceballoss-iMac.local>
2010-12-06 16:08:07 -0800

    port prediction sample script

---
+++ port_predict.rb
@@ -0,0 +1,34 @@
+require 'custom_socket'
+require 'port_client'
+require 'uri'
+
+class PortPredict
+  attr_reader(:port_client)
+  def initialize
+    @hosts = []
+    @hosts << "http://www.deusty.com/utilities/getMyIPAndPort.php"
+    @hosts << "http://www.robbiehanson.com:8080/utilities/getMyIPAndPort.php"
+  end
+
+  def start(lport = 0)
+    @hosts.each do |host|
+      active = CustomSocket.new
+      active.bind(lport)
+      puts "connecting"
+      active.connect("#{::URI.parse(host).host}", ::URI.parse(host).port)
+      req = "GET #{::URI.parse(host).path} HTTP/1.0\n\n"
+      active.print(req)
+      puts active.gets(nil)
+      active.close
+    end
+    
+    rescue Errno::ECONNREFUSED, Errno::EADDRNOTAVAIL => e
+      puts e.message
+      retry
+  end
+end
+
+if $0 == __FILE__
+  $:.push(File.dirname(__FILE__))
+  PortPredict.new.start(2009)
+end
