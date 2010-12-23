bcd59be303a32562701a9f599206fc0d24e5664d
Daniel Ceballos <daniel@li154-105.members.linode.com>
2010-12-23 05:05:35 +0000

    names

--- gateway_client.rb
+++ gateway_client.rb
@@ -1,14 +1,10 @@
 require 'peer'
 
 class GatewayClient
-  attr_reader(:port, :stunt_socket, :client)
+  attr_reader(:port, :peer_socket, :client)
 
   def initialize(port)
     @port = port
-  end
-
-  def get_client_socket
-    @client_socket = TCPSocket.new('localhost', port)
   end
 
   def start
@@ -16,15 +12,15 @@ class GatewayClient
     start_stunt
 
     begin
-      get_client_socket
-      while (sockets = IO.select([@stunt_socket, @client_socket]))
+      @client_socket = TCPSocket.new('localhost', port)
+      while (sockets = IO.select([@peer_socket, @client_socket]))
         sockets = sockets[0]
         sockets.each do |socket|                                                   
           data = socket.readpartial(512)
           if socket == @client_socket
             $stderr.puts "reading from client socket, writing to peer"
-            @stunt_socket.write data
-            @stunt_socket.flush
+            @peer_socket.write data
+            @peer_socket.flush
           else
             $stderr.puts "reading from peer socket, writing to client"
             @client_socket.write data
@@ -39,14 +35,14 @@ class GatewayClient
     rescue EOFError
       $stderr.puts "closing client socket"
       @client_socket.close
-      @stunt_socket.flush
+      @peer_socket.flush
       retry
     end
   end
 
   def start_stunt
     port_client   = PortClient.new("blastmefy.net:2000")
-    @stunt_socket = Peer.new(port_client).start("testy", 2005)
+    @peer_socket  = Peer.new(port_client).start("testy", 2005)
   end
 end
 
--- gateway_server.rb
+++ gateway_server.rb
@@ -1,15 +1,15 @@
 require 'peer_server'
 
 class GatewayServer
-  attr_reader(:port, :stunt_socket, :server)
+  attr_reader(:port, :peer_socket, :server)
 
   def initialize(port)
     @port = port
   end
 
   def start_stunt
-    port_client   = PortClient.new("blastmefy.net:2000")
-    @stunt_socket = PeerServer.new(port_client).start("testy", 2001)
+    port_client  = PortClient.new("blastmefy.net:2000")
+    @peer_socket = PeerServer.new(port_client).start("testy", 2001)
   end
 
   def start
@@ -34,13 +34,13 @@ class GatewayServer
   def handle_accept(client_socket)
     while true
       begin
-        sockets, dummy, dummy = IO.select([client_socket, @stunt_socket])
+        sockets, dummy, dummy = IO.select([client_socket, @peer_socket])
         sockets.each do |socket|
           data = socket.readpartial(512)
           if socket == client_socket
             $stderr.puts "reading from client socket, writing to peer"
-            @stunt_socket.write data
-            @stunt_socket.flush
+            @peer_socket.write data
+            @peer_socket.flush
           else
             $stderr.puts "reading from peer socket, writing to client"
             client_socket.write data
@@ -50,7 +50,7 @@ class GatewayServer
       rescue EOFError
         $stderr.puts "closing client socket"
         client_socket.close
-        @stunt_socket.flush
+        @peer_socket.flush
         break
       end
     end
