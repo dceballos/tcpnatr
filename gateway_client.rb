2e1032f7aae0b5319e1d59ff5cc8416639465049
Daniel Ceballos <daniel@li154-105.members.linode.com>
2010-12-24 23:37:40 +0000

    refined exception handling

--- gateway_client.rb
+++ gateway_client.rb
@@ -12,7 +12,7 @@ class GatewayClient
 
   def start_stunt
     port_client   = PortClient.new("blastmefy.net:2000")
-    @peer_socket  = PeerServer.new(port_client).start("testy", 2001)
+    @peer_socket  = Peer.new(port_client).start("testy", 2001)
   end
 
   def start
@@ -20,9 +20,7 @@ class GatewayClient
     start_stunt
 
     while (IO.select([@peer_socket]))
-      fork do
-        handle_accept
-      end
+      handle_accept
     end
   end
 
@@ -31,8 +29,7 @@ class GatewayClient
       @client_socket = TCPSocket.new(host, port)
       while (sockets = IO.select([@peer_socket, @client_socket]))
         timeout(10) do
-          sockets = sockets[0]
-          sockets.each do |socket|                                                   
+          sockets[0].each do |socket|                                                   
             data = socket.readpartial(4096)
             if socket == @client_socket
               $stderr.puts "reading from client socket, writing to peer"
@@ -48,18 +45,12 @@ class GatewayClient
           end
         end
       end
-    rescue Timeout::Error => e
+    rescue EOFError, Timeout::Error => e
       $stderr.puts e.message
       @peer_socket.flush
       @client_socket.flush
       @client_socket.close
-      retry
-    rescue Errno::ECONNRESET => e
-      $stderr.puts e.message
-      @peer_socket.flush
-      @client_socket.flush
-      retry
-    rescue EOFError => e
+    rescue IOError, Errno::ECONNRESET => e
       $stderr.puts e.message
       @peer_socket.flush
       @client_socket.close
