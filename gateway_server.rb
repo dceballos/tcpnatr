90a81b419a1abfbf970a6b1befcb1699bd7b2c72
Daniel Ceballos <daniel@li154-105.members.linode.com>
2010-12-29 07:08:46 +0000

    oob

--- gateway_server.rb
+++ gateway_server.rb
@@ -1,6 +1,7 @@
 require 'peer'
 require 'peer_server'
 require 'timeout'
+require 'fcntl'
 
 class GatewayServer
   attr_reader(:port, :peer_socket, :server)
@@ -11,7 +12,8 @@ class GatewayServer
 
   def start_stunt
     port_client  = PortClient.new("blastmefy.net:2000")
-    @peer_socket = Peer.new(port_client).start("testy", 2003)
+    @peer_socket = Peer.new(port_client).start("testy", 2005)
+    @peer_socket.fcntl(8, Process.pid)
   end
 
   def start
@@ -21,15 +23,25 @@ class GatewayServer
     @server = TCPServer.new(port)
     $stderr.puts "starting gateway server on port #{port}\n"
 
-    while (@client_socket = server.accept)
-      handle_accept
+    trap("URG") do
+      raise Sigurg
+    end
+
+    begin
+      while (@client_socket = server.accept)
+        handle_accept
+      end
+    rescue Sigurg
+      clean_state
+      retry
     end
   end
 
   def handle_accept
     begin
+      $stderr.puts "before first select"
       while(sockets = IO.select([@client_socket, @peer_socket]))
-        timeout(10) do
+        timeout(1) do
           sockets[0].each do |socket|
             data = socket.readpartial(4096)
             if socket == @client_socket
@@ -46,18 +58,37 @@ class GatewayServer
           end
         end
       end 
-    rescue EOFError, Timeout::Error => e
+    rescue IOError, Errno::ECONNRESET, Timeout::Error => e
       $stderr.puts e.message
-      @peer_socket.flush
-      @client_socket.flush
-      @client_socket.close
-    rescue IOError, Errno::ECONNRESET => e
-      $stderr.puts e.message
-      @peer_socket.flush
-      @client_socket.flush
-      @client_socket.close
+      clean_state
+      @peer_socket.send("e", Socket::MSG_OOB)
+    rescue Sigurg
+      clean_state
     end
   end
+
+  def clean_state
+    $stderr.puts "handling sigurg"
+    begin
+      timeout(1) do
+        while (socket = IO.select([@peer_socket]))
+          data = socket[0][0].readpartial(4096)
+          $stderr.puts "FLUSHING #{data}"
+        end
+      end
+    rescue Timeout::Error => e
+      $stderr.puts e.message
+    rescue EOFError, Errno::EPIPE => e
+      $stderr.puts e.message
+      retry
+    ensure
+      @client_socket.close unless @client_socket.closed?
+      @peer_socket.flush
+    end
+  end
+end
+
+class Sigurg < Exception
 end
 
 if $0 == __FILE__
